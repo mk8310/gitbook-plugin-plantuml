c9c62f4fac4c1aeaf10c6f88bf9607da6ae07ca2
wangmin <wangmin@eist.cn>
2019-09-05 09:54:10 +0800

    change plantuml server path to image type, because plantuml server path is hard code.

--- index.js
+++ index.js
@@ -12,81 +12,86 @@ require('shelljs/global');
 let options;
 
 const plantumlServerEscape = (block) => {
-  //UTF8
-  // const toDeflate = unescape(encodeURIComponent(block));
-  const deflated = zlib.deflateRawSync(block, {level: 9});
-  return plantUml.encode64(deflated);
+    //UTF8
+    // const toDeflate = unescape(encodeURIComponent(block));
+    const deflated = zlib.deflateRawSync(block, {level: 9});
+    return plantUml.encode64(deflated);
 };
 
 const createUML = async (content, uml, output) => {
-  const svgTag = `![](/${uml.svgPath})`;
-  content = content.replace(uml.rawBlock, svgTag);
-  await output.hasFile(uml.svgPath).then(exists => {
-    if (!exists) {
-      return new Promise(resolve => {
-        const client = options.protocol === 'https' ? https : http;
+    const svgTag = `![](/${uml.svgPath})`;
+    content = content.replace(uml.rawBlock, svgTag);
+    await output.hasFile(uml.svgPath).then(exists => {
+        if (!exists) {
+            return new Promise(resolve => {
+                const client = options.protocol === 'https' ? https : http;
 
-        let path = options.path;
-        let method = 'GET';
-        if (options.type === 'plantuml-server') {
-          path = options.path + '/' + plantumlServerEscape(uml.umlBlock);
-        } else {
-          method = 'POST';
+                let path = options.image_type;
+                let method = 'GET';
+                if (options.type === 'plantuml-server') {
+                    path = "/" + options.image_type + '/' + plantumlServerEscape(uml.umlBlock);
+                } else {
+                    method = 'POST';
+                }
+
+                const req = client.request({
+                    method: method,
+                    host: options.host,
+                    port: options.port,
+                    path: path
+                }, res => {
+                    const ws = fs.createWriteStream(output.resolve(uml.svgPath));
+                    res.pipe(ws);
+                    res.on('end', resolve);
+                });
+                if (method === 'POST') req.write(uml.umlBlock);
+                req.end();
+            });
         }
-
-        const req = client.request({method: method, host: options.host, port: options.port, path: path}, res => {
-          const ws = fs.createWriteStream(output.resolve(uml.svgPath));
-          res.pipe(ws);
-          res.on('end', resolve);
-        })
-        if (method === 'POST') req.write(uml.umlBlock);
-        req.end();
-      });
-    }
-  });
-  return content;
+    });
+    return content;
 };
 
 module.exports = {
-  hooks: {
-    'init': function() {
-      const output = this.output;
-      const config = this.config.values.pluginsConfig['plantuml-cloud'];
-      options = {
-        umlPath:  config.umlPath || 'assets/images/uml',
-        type: config.type || 'plantuml-service',
-        host: config.host || 'plantuml-service.herokuapp.com',
-        port: config.port || 80,
-        protocol: config.protocol ||'https',
-        path: config.path || '/svg',
-        blockRegex: config.blockRegex || '^```uml((.*[\r\n])+?)?```$'
-      };
-      const umlPath = output.resolve(options.umlPath);
-      mkdir('-p', umlPath);
-    },
-    'page:before': async function(page) {
-      let content = page.content;
-      const output = this.output;
+    hooks: {
+        'init': function () {
+            const output = this.output;
+            const config = this.config.values.pluginsConfig['plantuml'];
+            options = {
+                umlPath: config.umlPath || 'assets/images/uml',
+                type: config.type || 'plantuml-service',
+                host: config.host || 'plantuml-service.herokuapp.com',
+                port: config.port || 80,
+                protocol: config.protocol || 'https',
+                image_type: config.image_type || 'png',
+                blockRegex: config.blockRegex || '^```uml((.*[\r\n])+?)?```$'
+            };
+            const umlPath = output.resolve(options.umlPath);
+            mkdir('-p', umlPath);
+        },
+        'page:before': async function (page) {
+            let content = page.content;
+            const output = this.output;
 
-      const umls = [];
-      const re = new RegExp(options.blockRegex, 'img');
+            const umls = [];
+            const re = new RegExp(options.blockRegex, 'img');
 
-      while (match = re.exec(content)) {
-        const rawBlock = match[0];
-        const umlBlock = match[1].trim();
-        const md5 = crypto.createHash('md5').update(umlBlock).digest('hex');
-        const svgPath = path.join(options.umlPath, `${md5}.svg`);
-        umls.push({rawBlock, umlBlock,svgPath});
-      }
+            while (match = re.exec(content)) {
+                const rawBlock = match[0];
+                const umlBlock = match[1].trim();
+                const md5 = crypto.createHash('md5').update(umlBlock).digest('hex');
+                const svgPath = path.join(options.umlPath, `${md5}.` + options.image_type);
+                umls.push({rawBlock, umlBlock, svgPath});
+            }
 
-      for (const uml of umls) {
-        content = await createUML(content, uml, output);
-      }
+            for (const uml of umls) {
+                content = await createUML(content, uml, output);
+            }
 
-      page.content = content;
-      return page;
-    },
-    'page': page => page,
-    'page:after': page => page
-  }
+            page.content = content;
+            return page;
+        },
+        'page': page => page,
+        'page:after': page => page
+    }
 };
